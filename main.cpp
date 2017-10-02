858f335b42ab0401ddc547cf077355d6b10bb3ce
sergeyiandronov <sergeyiandronov@gmail.com>
2017-10-02 17:03:51 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,52 +2,51 @@
 #include <sstream>
 
 int main()
-{  
-	bool failure=false;
-	int max,maxsum;
-	int numbers[10];
-	int numbers1[10];
+{
+    bool failure=false;
+    int max,maxsum;
+    int numbers[10];
+    int numbers1[10];
+    while(!failure){
     for( std::string string; std::getline( std::cin, string ); ) {
-       
+
         std::istringstream stream( string );
-       
-        
+
+
         for( int i = 0; i < 10; ++i ) {
             if( !( stream >> numbers[ i ] ) ) {
                 failure = true;
                 break;
             }
-        }if(failure){break;}
+        }break;
     }
-	 for( std::string string; std::getline( std::cin, string ); ) {
-         for( int i = 0; i < 10; ++i ) {
+    if(failure){break;}
+    for( std::string string; std::getline( std::cin, string ); ) {
+        std::istringstream stream( string );
+        for( int i = 0; i < 10; ++i ) {
             if( !( stream >> numbers1[ i ] ) ) {
                 failure = true;
                 break;
             }
-        }if(failure){break;}}
-        max=numbers[0];
-        maxsum=max+numbers1[0];
-        for( int i = 1; i < 10; ++i ) {
-        	if(max<numbers[i]){
-        		max=numbers[i];
-        	}
-        	if(maxsum<max+numbers1[i]){
-        		maxsum=max+numbers1[i];
-        	}
+        }break;}break;}
+    max=numbers[0];
+    maxsum=max+numbers1[0];
+    for( int i = 1; i < 10; ++i ) {
+        if(max<numbers[i]){
+            max=numbers[i];
         }
-          if( !failure ) {
-        	
-            std::cout<< maxsum;
+        if(maxsum<max+numbers1[i]){
+            maxsum=max+numbers1[i];
         }
-        else {
-            std::cout << "An error has occured while reading numbers from line" << std::endl;
-        }
-        break;
-        }
-      
-      
     }
-    
-        
+    if( !failure ) {
+
+        std::cout<< maxsum;
+    }
+    else {
+        std::cout << "An error has occured while reading numbers from line" << std::endl;
+    }
+
 }
+
+
