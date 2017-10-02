989ae1ec56cdae0bdf7f30f6106b55b45298fbc3
sergeyiandronov <sergeyiandronov@gmail.com>
2017-10-02 16:57:33 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,7 +2,7 @@
 #include <sstream>
 
 int main()
-{   int where=1;
+{  
 	bool failure=false;
 	int max,maxsum;
 	int numbers[10];
@@ -10,22 +10,22 @@ int main()
     for( std::string string; std::getline( std::cin, string ); ) {
        
         std::istringstream stream( string );
-        switch(where){
-        	case 1:
+       
         
         for( int i = 0; i < 10; ++i ) {
             if( !( stream >> numbers[ i ] ) ) {
                 failure = true;
                 break;
             }
-        }where++;break;
-        case 2:
+        }if(failure){break;}
+    }
+	 for( std::string string; std::getline( std::cin, string ); ) {
          for( int i = 0; i < 10; ++i ) {
             if( !( stream >> numbers1[ i ] ) ) {
                 failure = true;
                 break;
             }
-        }
+        }if(failure){break;}}
         max=numbers[0];
         maxsum=max+numbers1[0];
         for( int i = 1; i < 10; ++i ) {
